12cdb5fab7f3bb814d4b8bbe5cd5afe4a450320c
vumilicaamm <milica.vucinic@code2.tech>
2025-07-23 15:25:37 +0200

    renamed memcached service

--- init-db/all_schemas.sql
+++ init-db/all_schemas.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE IF NOT EXISTS upmonthdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS upmsearch CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS upmonth_query_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS upmonth-text-extraction CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
